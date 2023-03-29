b718c19c8fb19fc1a5a298abbabaebf6b29ab9e1
arcadiobastidas <abastidas31@gmail.com>
2023-03-28 21:35:51 -0400

    Not finished, pending some changes

--- Assignment_4.1_DataStructures_SP23/Assignment_4._1_DataStructures_SP23.cpp
+++ Assignment_4.1_DataStructures_SP23/Assignment_4._1_DataStructures_SP23.cpp
@@ -8,12 +8,16 @@ using namespace std;
 
 int main()
 {
-   Assignment my_assignment("puta",Date(1,1,1));
+   Assignment my_assignment("Math101",Date(1,1,1));
    AssignmentList myList;
+   
 
    myList.addAssignment(my_assignment);
 
    myList.displayAssignments();
+
+   
+
 
 
  
